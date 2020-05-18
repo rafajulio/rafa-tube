07e63189692a0325e362a6f09e723907e38dc092
Rafael Julio <rafael.o.julio@gmail.com>
2020-05-18 11:25:11 -0300

    Mudança de nomeclatura da constante

--- src/webservices/youTubeServices.js
+++ src/webservices/youTubeServices.js
@@ -4,7 +4,7 @@ import {parse, toSeconds} from 'iso8601-duration';
 export async function getVideos(query) {
   const totalItems = 200;
   const maxItems = 50;
-  const key = 'AIzaSyBhIBVl9mxrXFF2o6UjMElrogJTZ8-_MpM'; // * Em uma build de produção, o ideal é utilizar uma variavel de ambiente para armazenar a chave de API.
+  const API_KEY = 'AIzaSyBhIBVl9mxrXFF2o6UjMElrogJTZ8-_MpM'; // * Em uma build de produção, o ideal é utilizar uma variavel de ambiente para armazenar a chave de API.
 
   let youTubeSearchItemsRetrieved = 0;
   let youTubeSearchItems = [];
@@ -14,7 +14,7 @@ export async function getVideos(query) {
 
   do {
     let response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q=${query}&key=${key}&maxResults=50&type=video&pageToken=${actualPageToken}`
+      `https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q=${query}&key=${API_KEY}&maxResults=50&type=video&pageToken=${actualPageToken}`
     );
     response = response.data;
     actualPageToken = response.nextPageToken;
@@ -38,7 +38,7 @@ export async function getVideos(query) {
   await Promise.all(
     videoIdArray.map(async (searchString) => {
       let response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet%2CcontentDetails&key=${key}&type=video&id=${searchString}`
+        `https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet%2CcontentDetails&key=${API_KEY}&type=video&id=${searchString}`
       );
       response = response.data;
       youTubeVideoItems = [...youTubeVideoItems, ...response.items];
